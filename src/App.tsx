86bc8217b4bb077211ebc992d8d2dfd5d5df5497
mayoumelon <mayoumelon@gmail.com>
2022-10-24 09:53:16 +0900

    Add React Query

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,14 @@
 import type { FC } from 'react';
-import { userData } from 'data';
-import { USER_COLUMNS } from 'domains';
-import DataTable from 'components/DataTable';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import Home from 'components/Home';
 
-const title = import.meta.env.VITE_APP_TITLE;
-console.dir(import.meta.env);
+const queryClient = new QueryClient();
 
 const App: FC = () => {
   return (
-    <div>
-      <h1 className="bg-red-500">{title}</h1>
-      <div className="m-4">
-        <DataTable data={userData} columns={USER_COLUMNS} />
-      </div>
-    </div>
+    <QueryClientProvider client={queryClient}>
+      <Home />
+    </QueryClientProvider>
   );
 };
 
---
+++ src/components/Home.tsx
@@ -0,0 +1,20 @@
+import type { FC } from 'react';
+import { userData } from 'data';
+import { USER_COLUMNS } from 'domains';
+import DataTable from 'components/DataTable';
+
+const title = import.meta.env.VITE_APP_TITLE;
+console.dir(import.meta.env);
+
+const Home: FC = () => {
+  return (
+    <div>
+      <h1 className="bg-red-500">{title}</h1>
+      <div className="m-4">
+        <DataTable data={userData} columns={USER_COLUMNS} />
+      </div>
+    </div>
+  );
+};
+
+export default Home;
